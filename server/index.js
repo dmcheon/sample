153b6a10126ab7d4e7191046fd3becd41d3db261
dongmi <dmcheon@gmail.com>
2022-02-25 17:46:12 +0900

    upload files

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+//connect with mongoDB
+
+const url = 'mongodb://127.0.0.1:27017/testdb';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
